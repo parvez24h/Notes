a671b3ca874def2532b2c0773728d1d8495d66f0
Parvez <parvezz24h@gmail.com>
2025-05-19 09:46:37 +0600

    User Registration

--- lib/views/registrationPage.dart
+++ lib/views/registrationPage.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+
+import 'homePage.dart';
 
 
 class RegistrationPage extends StatefulWidget {
@@ -9,17 +12,211 @@ class RegistrationPage extends StatefulWidget {
 }
 
 class _RegistrationPageState extends State<RegistrationPage> {
+
+  var nameEditingController = TextEditingController();
+  var emailEditingController = TextEditingController();
+  var passEditingController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          title: Text("Registration", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
-          backgroundColor: Colors.pink,
-        ),
-        body: Center(
-            child: Text("Registration Page", style: TextStyle(color: Colors.black, fontSize: 25),)
-        )
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text("Registration", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+        backgroundColor: Colors.pink,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+
+          SizedBox(height: 20,),
+
+          Row(
+            children: [
+              SizedBox(width: 20,),
+              Expanded(
+                child: TextField(
+                  keyboardType: TextInputType.text,
+                  controller: nameEditingController,
+                  enabled: true,
+                  decoration: InputDecoration(
+                      hintText: "Name: ",
+                      prefixStyle: TextStyle(color: Colors.grey),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(
+                              color: Colors.blue
+                          )
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(
+                              color: Colors.blue
+                          )
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(
+                              color: Colors.black
+                          )
+                      ),
+                      disabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(
+                              color: Colors.yellow
+                          )
+                      )
+                  ),
+                ),
+              ),
+              SizedBox(width: 20,)
+            ],
+          ),
+
+          SizedBox(height: 20,),
+
+          Row(
+            children: [
+              SizedBox(width: 20,),
+              Expanded(
+                child: TextField(
+                  keyboardType: TextInputType.text,
+                  controller: emailEditingController,
+                  enabled: true,
+                  decoration: InputDecoration(
+                      hintText: "Email: ",
+                      prefixStyle: TextStyle(color: Colors.grey),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(
+                              color: Colors.blue
+                          )
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(
+                              color: Colors.blue
+                          )
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(
+                              color: Colors.black
+                          )
+                      ),
+                      disabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(
+                              color: Colors.yellow
+                          )
+                      )
+                  ),
+                ),
+              ),
+              SizedBox(width: 20,)
+            ],
+          ),
+
+          SizedBox(height: 20,),
+
+          Row(
+            children: [
+              SizedBox(width: 20,),
+              Expanded(
+                child: TextField(
+                  controller: passEditingController,
+                  enabled: true,
+                  obscureText: true,
+                  onChanged: (value) {
+                    print("Change Text: $value");
+                  },
+
+                  decoration: InputDecoration(
+                      hintText: "Password:",
+                      prefixStyle: TextStyle(color: Colors.grey),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(
+                              color: Colors.blue
+                          )
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(
+                              color: Colors.blue
+                          )
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(
+                              color: Colors.black
+                          )
+                      ),
+                      disabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(
+                              color: Colors.pink
+                          )
+                      )
+                  ),
+                ),
+              ),
+              SizedBox(width: 20,)
+            ],
+          ),
+
+          SizedBox(height: 20,),
+
+          Row(
+            children: [
+
+              Expanded(
+                  flex: 1,
+                  child: SizedBox(width: 20,)
+              ),
+              Expanded(
+                  flex: 2,
+                  child: ElevatedButton(
+                      onPressed: () async{
+
+
+                        String name = nameEditingController.text.toString();
+                        String email = emailEditingController.text.toString();
+                        String password = passEditingController.text.toString();
+
+                        FirebaseFirestore.instance.collection("users").doc().set(
+                            {
+                              "name":name,
+                              "email": email,
+                              "password": password
+                            }).then((_)=>{
+                              print("User registered")
+                            });
+
+                        Navigator.pushReplacement(context, MaterialPageRoute(builder: (_)=>const HomePage()));
+
+                      },
+                      child: Text("Register", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18, color: Colors.blue),)
+                  )
+              ),
+
+              Expanded(
+                  flex: 1,
+                  child: SizedBox(width: 20,)
+              ),
+            ],
+          ),
+
+          SizedBox(height: 20,),
+
+          Text("Already register Please Login")
+
+
+
+
+
+        ],
+      ),
 
     );
   }
--- lib/views/splashScreen.dart
+++ lib/views/splashScreen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:notes/views/homePage.dart';
 import 'package:notes/views/loginPage.dart';
+import 'package:notes/views/registrationPage.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -23,7 +24,7 @@ class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderSt
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
     Future.delayed(const Duration(seconds: 3), (){
       Navigator.of(context).pushReplacement(
-        MaterialPageRoute(builder: (_)=>mAuth?const HomePage():const HomePage())
+        MaterialPageRoute(builder: (_)=>mAuth?const HomePage():const RegistrationPage())
       );
     });
   }
